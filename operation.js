9e6ad7d1c5afc94f26a5283672f6d37a0c1e4e5a
_iamsb3_ <get.input.2025@gmail.com>
2025-01-16 16:56:14 +0000

    simple operation

---
+++ operation.js
@@ -0,0 +1,16 @@
+
+let score =102;
+
+let bonus=25;
+
+let totalScore=score + bonus;
+let addition =4+5; 
+let subtract =9-3;
+let mult= 3*5;
+
+let divi =8/2;
+let reminder=8%2;
+
+let expo = 2** 2;// how power you want 
+console.log(expo);
+
