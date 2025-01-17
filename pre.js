e6519f6b4e8f8c9569595a49078528a2601fa35b
_iamsb3_ <get.input.2025@gmail.com>
2025-01-17 17:07:46 +0000

    premitive datatype

---
+++ pre.js
@@ -0,0 +1,19 @@
+//today again we are discuss about datatype but further more in details 
+// data type is divided into 1.premitive (predefined datatype) 2.non-premitive(user have to define it when user work on them)
+// premitive datatype are 1.number 2.string 3.null 4.undefined 5.symbol 6.boolean(true and false)
+//non-premitive datatype are divided in OBJECT . and OBJECT are also divded into sub topics are array,functions,object itself too.
+
+
+//typeof()- to know the type of datatype
+
+let  balance=120;
+console.log(balance);
+console.log(typeof(balance));
+// its give the typeof method 
+
+//another way to know datatype
+
+let anotherBalance = new Number(107);
+console.log( anotherBalance);
+// lets find out what's is the difference between balance and anotherBalance:
+
