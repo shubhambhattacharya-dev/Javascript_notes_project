960bc7d089e4adcbe0a29f68b302ce127ac8200c
_iamsb3_ <get.input.2025@gmail.com>
2025-01-21 17:34:42 +0000

    continue in morning..sorry

---
+++ non-pre.js
@@ -0,0 +1,17 @@
+//object
+
+// SYNTAX:
+
+let obj={};// its a valid object
+
+console.log(obj);
+console.log(typeof(obj));//object
+
+// for better we use an example
+//user loggedin  : alway use key value pair
+let userName={
+       firstName:"shubham Bhattacharya",
+       loggedIn:true               // imp: TO ASSIGN VALUE USE " : "(USE COLON) AND FOR SEPARATION USE " ,"(COMMA)
+};
+
+console.log(userName);
