f75fdba2b4346b69dec824ca13bcb4b05f5eb0d6
_iamsb3_ <get.input.2025@gmail.com>
2025-02-03 11:21:40 +0000

    basic code only

---
+++ string.js
@@ -0,0 +1,6 @@
+let fristName="shubham";
+let lastName="bhattacharya";
+let age= 19;
+let isheRich=true;
+
+console.log("this person is " + fristName + " and their age is "+ age);
---
+++ var.js
@@ -0,0 +1,5 @@
+// let,var,const
+//var was the original way tp define variable in js
+//let use now
+const a=1;//for sefety 
+console.log(a);
