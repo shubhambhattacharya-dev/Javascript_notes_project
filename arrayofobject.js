d1d1cf267b7d7570428fc9e017ad5c7e6e09432f
_iamsb3_ <get.input.2025@gmail.com>
2025-02-04 05:42:36 +0000

    arrayofobject

---
+++ arrayofobject.js
@@ -0,0 +1,31 @@
+
+const users = [
+    {
+      firstName: "shubham",
+      lastName: "Bhattacharya",  // Changed "Lastname" to "lastName"
+      gender: "male",
+    },
+    {
+      firstName: "prince",
+      lastName: "kumar",  // Consistent case
+      gender: "male",
+    },
+    {
+      firstName: "riya",
+      lastName: "kumari",
+      gender: "female",
+    },
+    {
+      firstName: "sonali",
+      lastName: "raj",
+      gender: "female",
+    }
+  ];
+  
+  console.log(users[1]["gender"]); // Correct usage of square brackets
+  for(let i=0;i<users.length;i++){
+    if(users[i]["gender"]=="male"){
+      console.log(users[i]["firstName"]);
+    }
+  }
+  
