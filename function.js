547f14c75134f6117d16b315a64c396942f97cbe
_iamsb3_ <get.input.2025@gmail.com>
2025-02-04 17:05:44 +0000

    functions and objects

---
+++ function.js
@@ -0,0 +1,11 @@
+//
+function sum(a,b) {// the thread reach here and check oh! its a function. and run i t yet just store   
+    const sumValue=(a+b);//third here check operation
+    return sumValue;//here return value store in value variable last 
+}
+
+const value=sum(4,6);// second thread reach here sum is call
+const value2=sum(97,62);
+
+console .log(value);// here print it in console
+console .log(value2);
