6aa3cafa44485496c04a9162fe801a58cf078d6a
_iamsb3_ <get.input.2025@gmail.com>
2025-02-04 04:16:20 +0000

    complex pre

---
+++ object.js
@@ -0,0 +1,10 @@
+const userDeatail= {
+    //key value pair 
+    firstName:"shubham",
+    lastName:"Bhattacharya",
+    gender:"Male",
+
+};
+
+console.log(userDeatail.gender);//method
+console.log(userDeatail["firstName"]);// space cause undefine
