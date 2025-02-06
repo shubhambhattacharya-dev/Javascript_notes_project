a4fd15330208f3bf974beb7993b222cd42342ce4
_iamsb3_ <get.input.2025@gmail.com>
2025-02-06 08:04:12 +0000

    settimeout.js

---
+++ settimeout.js
@@ -0,0 +1,11 @@
+//setTimeout
+function greeet(){
+    console.log("hello world")
+}
+
+setTimeout ( greeet, 1 * 1000)//callback
+// global function that js provide you second argument  a number that represent a num in sec time that wait you before first argument to run(after 1 sec meassge delcerd on console panel)
+
+setInterval(greeet,1*1000);//similare to setTimeout just call function  some certain interval here i 1 sec// to stopping it use cntl+c // on terminal
+
+// 
