37ef9b734b995e7b35fae3c345366ce8e489dbd6
_iamsb3_ <get.input.2025@gmail.com>
2025-02-11 03:05:57 +0000

    num function

---
+++ allnumberfunction.js
@@ -0,0 +1,5 @@
+//explainparseint
+console.log (parseInt("42"));//global function its convert string to number 
+
+console.log(parseInt("42px"));//only give integer 
+console.log(parseInt("3.14"));//float to int paring a number into integer
